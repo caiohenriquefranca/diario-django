4530d3c02c33491bbb2952a6c366a53781d5254c
Caio Henrique <caiohenriquefrancav@icloud.com>
2025-01-08 18:03:45 -0300

    first commit

---
+++ diario/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+def home(request):
+    return render(request, 'home.html')
+
+def escrever(request):
+    if request.method == 'GET':
+        return render(request, 'escrever.html')
+    elif request.method == 'POST':
+        titulo = request.POST.get('titulo')
+        tags = request.POST.getlist('tags')
+        pessoas = request.POST.getlist('pessoas')
+        texto = request.POST.get('texto')
+        return HttpResponse(f'{titulo} - {tags} - {pessoas} - {texto}')
+
+def cadastrar_pessoa(request):
+    if request.method == 'GET':
+        return render(request, 'pessoa.html')
